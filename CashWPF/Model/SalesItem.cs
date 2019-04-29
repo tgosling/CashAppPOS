bc84cb39f031cbd45ee8c778bb678193b40734e8
Tyler Gosling <t.gosling61@gmail.com>
2019-04-29 00:08:36 -0400

    first commit, GUI started

---
+++ CashWPF/Model/SalesItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashWPF.Model
+{
+    class SalesItem
+    {
+        public string saleItem { get; set; }
+        public int itemQty { get; set; }
+        public string itemSts { get; set; }
+        public int itemAmt { get; set; }
+    }
+}
---
+++ CashWPF/ViewModel/ViewModelMain.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CashWPF.Model;
+
+namespace CashWPF.ViewModel
+{
+    class ViewModelMain
+    {
+        public ObservableCollection<SalesItem> TransacItem { get; set; }
+
+        public ViewModelMain()
+        {
+            TransacItem = new ObservableCollection<SalesItem>();
+        }
+    }
+
+  
+}
