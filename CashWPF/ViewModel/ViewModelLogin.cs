f809f1b9cab1a39bcaf28801a9535a3f847c477c
Tyler Gosling <t.gosling61@gmail.com>
2019-05-07 16:23:15 -0400

    ViewModel for login

---
+++ CashWPF/ViewModel/ViewModelLogin.cs
@@ -0,0 +1,52 @@
+﻿using CashWPF.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Newtonsoft.Json;
+
+namespace CashWPF.ViewModel
+{
+    class ViewModelLogin
+    {
+        public List<Employee> empList { get; set; }
+        public int loggedInUser { get; set; }
+
+        //Checks to see if the employee is valid in list of employees
+        public bool checkLogin(string userN, string passW)
+        {
+            int count = 0;
+            foreach (Employee hold in empList)
+            {
+                if (hold.empUsrName == userN && hold.empUsrName == passW)
+                {
+                    loggedInUser = count;
+                    return true;
+                }
+                count++;
+            }
+            return false;
+        }
+
+
+
+        public void loadUsers()
+        {
+            try
+            {
+                if (File.Exists("users.json"))
+                {
+                    string jsonString = File.ReadAllText("users.json");
+                    empList = JsonConvert.DeserializeObject<List<Employee>>(jsonString);
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+        }
+    }
+}
