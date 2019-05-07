38bae3b97411a4ef64ff352dda474aebbee37368
Tyler Gosling <t.gosling61@gmail.com>
2019-05-07 15:48:16 -0400

    Added employee class for login criteria

---
+++ CashWPF/Model/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashWPF.Model
+{
+    class Employee
+    {
+        public string empUsrName { get; set; }
+        public string empPassW { get; set; }
+
+        public Employee(string usrName, string passW)
+        {
+            this.empUsrName = usrName;
+            this.empPassW = passW;
+        }
+    }
+}
